a9ef272f1db70fc5e16fd1a8f36be9f49bed5be5
khalidabuhakmeh <khalidabuhakmeh@gmail.com>
2026-07-09 06:59:13 -0400

    chore: bump version to 1.1.1

--- src/NuGetAssemblyMcp/Program.cs
+++ src/NuGetAssemblyMcp/Program.cs
@@ -18,7 +18,7 @@ builder.Services
         options.ServerInfo = new Implementation
         {
             Name = "NuGetAssemblyMcp",
-            Version = "1.0.0"
+            Version = "1.1.1"
         };
     })
     .WithStdioServerTransport()
